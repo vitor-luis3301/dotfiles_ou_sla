a74b6cd94157d029c47bad90dabe957753f0155c
vitor-luis3301 <superpowervitor2@gmail.com>
2025-10-28 22:27:34 -0300

    Added Rofi as launcher + Corrections on setup scripts

--- setup.sh
+++ setup.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 # Install yay
-sudo pacman -S --needed git base-devel
+sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 # Update packages
 sudo pacman -Syu
-yay -Syu
+yay -Syu 
 # Install general system apps on the pacman repository
-sudo pacman -S base-devel hyprland dolphin polkit-kde-agent ly qt5-wayland qt6-wayland grim slurp wofi xdg-desktop-portal-hyprland swaync hyprpaper hyprlock gvfs gvfs-mtp gvfs-smb unzip p7zip unrar waybar blueman foot cava flatpak wl-clipboard zathura zathura-pdf-poppler vlc pavucontrol cliphist wlsunset playerctl bluez bluez-utils pipewire-pulse pipewire-bluetooth wireplumber swayimg fastfetch xdg-desktop-portal ninja cmake xdg-user-dirs
+sudo pacman -S --needed --noconfirm ninja cmake ly dolphin grim slurp rofi \
+hyprland hyprshade hypridle hyprpaper hyprlock hyprshot hyprpicker hyprland-qt-support hyprland-qutils hyprsunset \
+swaync swayimg waybar cliphist foot alacritty cava fastfetch cowsay gnome-calendar gnome-calculator \
+zathura zathura-pdf-poppler vlc gvfs gvfs-mtp gvfs-smb xarchiver unzip p7zip unrar blueman flatpak wine \
+wl-clipboard pavucontrol playerctl bluez bluez-utils wireplumber polkit-kde-agent \
+qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-user-dirs xdg-desktop-portal pipewire-pulse
+
+xdg-user-dirs-update
 
 # Install nerd font
 sudo pacman -Ss jetbrains nerd
 sudo pacman -S ttf-jetbrains-mono-nerd
 
 # Install with AUR
-yay -S vicinae-bin wlogout hyprshade hypridle
+yay -S vicinae-bin wlogout cbonsai-git kiview cmatrix-git
 
 # Install various flatpaks
-flatpak install io.github.nyre221.kiview
 flatpak install flathub app.zen_browser.zen
 flatpak install flathub com.brave.Browser
 flatpak install flathub com.valvesoftware.Steam
 flatpak install flathub com.heroicgameslauncher.hgl
 flatpak install flathub org.vinegarhq.Sober
-flatpak install flathub md.obsidian.Obsidian
 flatpak install flathub net.pcsx2.PCSX2
 flatpak install flathub io.github.giantpinkrobots.flatsweep
 flatpak install flathub org.srb2.SRB2
 flatpak install flathub com.spotify.Client
-flatpak install flathub org.winehq.Wine
-flatpak install flathub org.kde.spectacle
-flatpak install flathub org.gnome.Calendar
-flatpak install flathub org.gnome.FileRoller
-flatpak install flathub org.gnome.Calculator
 
 # Install ollama
 curl -fsSL https://ollama.com/install.sh | sh
@@ -46,14 +46,13 @@ curl -fsSL https://ollama.com/install.sh | sh
 curl -LO https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz
 tar -xvzf nvim-linux-x86_64.tar.gz
 sudo mkdir /usr/bin/nvim
-sudo mv ./nvim-linux-x86_64/* /usr/bin/nvim/
-rm -rf ./nvim-linux-x86_64*
-
+sudo mv ./nvim-linux-x86_64/* /usr/bin/nvim
 # Installing LazyVim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 mv ./wallpapers ~/Pictures/
+
 mv ./confs/fastfetch ~/.config/
 mv ./confs/hypr ~/.config/
 mv ./confs/waybar ~/.config/
@@ -64,3 +63,5 @@ echo "fastfetch" >> ~/.bashrc
 
 sudo systemctl enable ly
 sudo systemctl start ly
+
+rm -rf ./nvim-linux-x86_64* ./cmatrix
--- setup_minimal.sh
+++ setup_minimal.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 # Install yay
-sudo pacman -S --needed git base-devel
+sudo pacman -S --needed --noconfirm base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 # Update packages
 sudo pacman -Syu
-yay -Syu
+yay -Syu 
 # Install general system apps on the pacman repository
-sudo pacman -S base-devel hyprland dolphin polkit-kde-agent ly qt5-wayland qt6-wayland grim slurp wofi xdg-desktop-portal-hyprland swaync hyprpaper hyprlock gvfs gvfs-mtp gvfs-smb unzip p7zip unrar waybar blueman foot cava flatpak wl-clipboard zathura zathura-pdf-poppler vlc pavucontrol cliphist wlsunset playerctl bluez bluez-utils pipewire-pulse pipewire-bluetooth wireplumber swayimg fastfetch xdg-desktop-portal ninja cmake xdg-user-dirs
+sudo pacman -S --needed --noconfirm ninja cmake ly dolphin grim slurp rofi \
+hyprland hyprshade hypridle hyprpaper hyprlock hyprshot hyprpicker hyprland-qt-support hyprland-qutils hyprsunset \
+swaync swayimg waybar cliphist foot alacritty cava fastfetch cowsay \
+zathura zathura-pdf-poppler vlc gvfs gvfs-mtp gvfs-smb unzip p7zip unrar blueman flatpak \
+wl-clipboard pavucontrol playerctl bluez bluez-utils wireplumber polkit-kde-agent \
+qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-user-dirs xdg-desktop-portal pipewire-pulse
+
+xdg-user-dirs-update
 
 # Install nerd font
 sudo pacman -Ss jetbrains nerd
 sudo pacman -S ttf-jetbrains-mono-nerd
 
 # Install with AUR
-yay -S vicinae-bin wlogout hyprshade hypridle
+yay -S vicinae-bin wlogout cbonsai-git
+
+# Install ollama
+curl -fsSL https://ollama.com/install.sh | sh
 
 # Neovim
 
@@ -25,14 +35,20 @@ yay -S vicinae-bin wlogout hyprshade hypridle
 curl -LO https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz
 tar -xvzf nvim-linux-x86_64.tar.gz
 sudo mkdir /usr/bin/nvim
-sudo mv ./nvim-linux-x86_64/* /usr/bin/nvim/
-rm -rf ./nvim-linux-x86_64*
-
+sudo mv ./nvim-linux-x86_64/* /usr/bin/nvim
 # Installing LazyVim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
+git clone https://github.com/abishekvashok/cmatrix.git
+cd cmatrix
+mkdir -p Build && cd Build
+cmake ..
+make
+make install
+
 mv ./wallpapers ~/Pictures/
+
 mv ./confs/fastfetch ~/.config/
 mv ./confs/hypr ~/.config/
 mv ./confs/waybar ~/.config/
@@ -43,3 +59,5 @@ echo "fastfetch" >> ~/.bashrc
 
 sudo systemctl enable ly
 sudo systemctl start ly
+
+rm -rf ./nvim-linux-x86_64* ./cmatrix
