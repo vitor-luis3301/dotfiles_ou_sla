579b96ca518eba5d46621d92113d78f252f1f1d7
vitor-luis3301 <superpowervitor2@gmail.com>
2025-10-30 10:54:08 -0300

    Added new waybar theme

---
+++ confs/waybar/scripts/cava.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# cava -p ~/.config/cava/cava_waybar.conf | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
+cava -p ~/.config/cava/cava_waybar.conf | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
