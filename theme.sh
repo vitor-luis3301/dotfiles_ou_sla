9179f4b2c5bc5a8d2b626e04ff97cc0de3bb789c
vitor-luis3301 <superpowervitor2@gmail.com>
2025-11-03 20:20:20 -0300

    Updated theme.sh to support single image 'themes' (I have to figure out a better way to use this)

--- theme.sh
+++ theme.sh
@@ -13,11 +13,13 @@ else
   if [ -f $THEME_FILE ]; then
     IFS=":" read -r THEME COLOR < $THEME_FILE
 fi
-declare -a colors
 
-IMAGE="$HOME/Pictures/wallpapers/$THEME/$THEME-$COLOR.jpg"
-
-hellwal -i "$IMAGE"
+if [ -f $THEME ]; then
+  IMAGE="$HOME/Pictures/wallpapers/$THEME/$THEME-$COLOR.jpg"
+  hellwal -i "$IMAGE"
+else
+  IMAGE="$HOME/Pictures/wallpapers/$THEME.jpg"
+  hellwal -i $IMAGE --$COLOR
 
 hyprctl reload
 hyprctl hyprpaper reload , "$IMAGE"
