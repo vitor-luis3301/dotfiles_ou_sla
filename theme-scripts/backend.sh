e2b39c4a4c91765d503c6676971c87854db8fea8
vitor-luis3301 <superpowervitor2@gmail.com>
2025-11-11 08:54:50 -0300

    Added dark theme switching for apps as well

--- theme-scripts/backend.sh
+++ theme-scripts/backend.sh
@@ -5,5 +5,14 @@ hellwal -i $2 --$1
 hyprctl reload
 hyprctl hyprpaper reload ,$2
 
+if [ $2 = "dark" ]; then
+  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+  gsettings set org.gnome.desktop.interface gtk-theme adwaita-dark
+  # gsettings set org.gnome.desktop.interface icon-theme breeze-dark
+else
+  gsettings set org.gnome.desktop.interface color-scheme default
+  gsettings set org.gnome.desktop.interface gtk-theme adwaita
+  # gsettings set org.gnome.desktop.interface icon-theme breeze-dark
+
 killall waybar
 hyprctl dispatch exec waybar
